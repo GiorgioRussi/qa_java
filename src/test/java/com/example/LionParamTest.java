38a5d0d7ccbe9e362eab4bbba1d4d903ad207b31
GiorgioRussi <shonetod7775@gmail.com>
2024-05-19 19:51:21 +0300

    add jacoco report

---
+++ src/test/java/com/example/LionParamTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+@RunWith(Parameterized.class)
+public class LionTest {
+
+    @Test
+    public void getKittens() {
+    }
+
+    @Test
+    public void doesHaveMane() {
+    }
+
+    @Test
+    public void getFood() {
+    }
+}
